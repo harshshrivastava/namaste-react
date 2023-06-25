9c2753a6bdf6e05d0c6249a22f8081c31b0da74b
harsh <harshshrivastav94@gmail.com>
2023-06-25 15:11:33 +0530

    Study

--- App.js
+++ App.js
@@ -1,40 +1,20 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-/**
- * <div id="parent">
- *  <div id="child">
- *      <h1></h1>
- *      <h2></h2>
- *  </div>
- * <div id="child2">
- *      <h1></h1>
- *      <h2></h2>
- *  </div>
- * </div>
- */
-var divParent = React.createElement(
-    "div", 
-    { id: "parent" },
-    [
-        React.createElement
-        (
-            "div",
-            {id:"child"},
-            [
-                React.createElement("h1",{},"H1 from child"),
-                React.createElement("h2",{},"H2 from child")
-            ]
-        ),
-        React.createElement
-        (
-            "div",
-            {id:"child2"},
-            [
-                React.createElement("h1",{},"H1 from child2"),
-                React.createElement("h2",{},"H2 from child2")
-            ]
-        )
-    ]
+
+var Title = () =>(
+    <h1 className="head" tabIndex="5">
+       I am from Title
+    </h1>
 )
-var root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(divParent)
+var HeadingFunctionalComponent = () => (
+    <div id="container">
+        <Title/>
+        <h1>React Functional Component.</h1>
+    </div>
+)
+
+
+var root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<HeadingFunctionalComponent />)
--- Readme.md
+++ Readme.md
@@ -1,1 +1,13 @@
 # Namaste React
+
+# Parcel
+- Dev build
+- Local Server
+- HMR -> Hot Module Replacement. If we make change in file and then save it, it automatically refresh browser.
+         Because parcel is using HMR.
+- File Watching Algorithm -> written in C++
+- Caching - Faster Builds
+- Performs Image Optimization
+- Minification
+- Bundling
+- Compressing
